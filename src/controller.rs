ce10d8386a70e03913968f38ecea6ebbb0c8593d
File Tibor József <filetibor2002@gmail.com>
2024-12-22 09:54:57 +0100

    Create frontend

--- src/controller.rs
+++ src/controller.rs
@@ -1,10 +1,12 @@
 use rocket::{Build, Rocket};
+use rocket::fs::{relative, FileServer};
 
 mod star;
 mod constellation;
 
 pub fn mount_routes(rocket: Rocket<Build>) -> Rocket<Build> {
     rocket
-        .mount("/star", star::routes())
-        .mount("/constellation", constellation::routes())
+        .mount("/", FileServer::from(relative!("public")))
+        .mount("/api/star", star::routes())
+        .mount("/api/constellation", constellation::routes())
 }
